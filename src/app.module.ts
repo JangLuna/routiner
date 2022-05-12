11c269aae0f80cc03ce537ed5f40bf84dc467aa0
lunaJYJ <devmonarch0115@gmail.com>
2022-05-12 11:18:58 +0900

    typeorm setting change

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AtomModule } from './atom/atom.module';
-import { typeORMConfig } from './configs/typeorm.config';
 import { Atom } from './entities/atom.entity';
 import { RoutainLog } from './entities/routain-log.entity';
 import { Routain } from './entities/routain.entity';
@@ -11,15 +10,24 @@ import { Todo } from './entities/todo.entity';
 import { RoutainModule } from './routain/routain.module';
 import { TodoModule } from './todo/todo.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigurationModule } from './configuration/configuration.module';
 
 @Module({
   imports: [
     AtomModule,
     RoutainModule,
     TodoModule,
-    TypeOrmModule.forRoot(typeORMConfig),
+    TypeOrmModule.forRoot({
+      type: 'mariadb',
+      host: process.env.DB_ENDPOINT,
+      port: 3306,
+      username: process.env.DB_USER,
+      password: process.env.DB_PW,
+      database: 'Routainer',
+    }),
     TypeOrmModule.forFeature([Routain, RoutainLog, Todo, Atom]),
     AuthModule,
+    ConfigurationModule,
   ],
   controllers: [AppController],
   providers: [AppService],
